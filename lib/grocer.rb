85eef2edf1d03125172007924b56a839bd95d606
Roman Turner <rn.turner@live.com>
2021-01-23 20:00:02 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,7 +3,7 @@ def find_item_by_name_in_collection(name, collection)
   item_hash_found = {}
   collection.each do |hash|
     hash.each do |key, value|
-        if value = name 
+        if key = name 
           item_hash_found = hash
         
       end 
